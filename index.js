5c2e60061f60485ec492f04f595fb4b562dead6b
Sheryl Boughton <sherpbo@gmail.com>
2015-10-23 01:42:57 -0700

    include basic show/hide of founder bios

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //just waiting for clarification. :)
+
+$(function(){
+  console.log('ready');
+  init();
+});
+
+function init(){
+  $('.founder-title').on('click', function(){
+    var $header = $(this);
+    var detailDisplay = $(this).siblings().css('display');
+
+    if(detailDisplay !== 'none'){
+      $($header).siblings().css('display', 'none');
+    } else {
+      $($header).siblings().css('display', 'block');
+    }
+    
+  });
+}
